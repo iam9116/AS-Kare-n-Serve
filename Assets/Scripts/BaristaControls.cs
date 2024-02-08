7c9f7bc7d1f1e864ff9a45b1e189021e17c85f4f
I Love Iced Coffee <148917437+iam9116@users.noreply.github.com>
2024-02-08 18:07:34 -0500

    Started Coding
    Yaey

---
+++ Assets/Scripts/BaristaControls.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaristaControls : MonoBehaviour
+{
+    public float speed = 10f;
+
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0.0f);
+        rb.velocity = movement * speed * Time.deltaTime;
+    }
+}
