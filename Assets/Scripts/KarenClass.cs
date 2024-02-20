c64f09b0c643312fe62738cd8de2e32973285489
I Love Iced Coffee <148917437+iam9116@users.noreply.github.com>
2024-02-20 18:02:17 -0500

    QWERTYUIOP
    QAZXSWEDCVFRTGBNHYUJM<KIOL>?:P{"!@#$%$%^&**&*&^%$#$^%&*(($^&**^&*&&*^^*@^&%^&#(#()@#()!!@#!@#%^!@%#!^%@@$$#@$@&!@#@#^!^#&*@(

--- Assets/Scripts/KarenClass.cs
+++ Assets/Scripts/KarenClass.cs
@@ -5,12 +5,19 @@ using TMPro;
 
 public class KarenClass : MonoBehaviour
 {
-    private TMPro.TextMeshProUGUI text;
+    public List<string> KarenSpeech;
+    public float waitTime = 45f;
+
+    private TMPro.TextMeshPro text;
 
     // Start is called before the first frame update
     void Start()
     {
-        text = GetComponentInChildren<TextMeshProUGUI>();    
+        text = GetComponentInChildren<TextMeshPro>();
+
+        if (KarenSpeech != null){
+            ChangeText(KarenSpeech[0]);
+        }
     }
 
     // Update is called once per frame
@@ -21,6 +28,7 @@ public class KarenClass : MonoBehaviour
 
     public void ChangeText(string newText)
     {
+        if (text == null) return;
         text.text = newText;
     }
 }
--- Assets/Scripts/MotherKarenCreator.cs
+++ Assets/Scripts/MotherKarenCreator.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class MotherKarenCreator : MonoBehaviour
 {
     public KarenClass[] karens;
+
+
     // Start is called before the first frame update
     void Start()
     {
         int randomIndex = Random.Range(0, karens.Length);
         KarenClass randomObject = karens[randomIndex];
-        var position = new Vector3(Random.Range(-10, 10), Random.Range(-10, 10), 0);
-        Instantiate(randomObject, position, Quaternion.identity);
+        Vector3 position = new Vector3(Random.Range(-10, 10), Random.Range(-10, 10), 0);
+        Instantiate(karens[randomIndex], position, Quaternion.identity);
     }
 
     // Update is called once per frame
