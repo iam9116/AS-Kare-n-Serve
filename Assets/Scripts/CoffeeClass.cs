2681bd3c265ce75fdb9cf167e80b41d2a801e92a
I Love Iced Coffee <ashtonshyti@outlook.com>
2024-02-27 18:00:41 -0500

    QAZGYUFTDXFGJUKYFTDSZXFG
    YGFTDFGXDCH

---
+++ Assets/Scripts/CoffeeClass.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CoffeeType
+{
+    BLACK = 0,
+    ESPRESSO,
+    LATTE,
+    MACCHIATO
+}
+
+public class CoffeeClass : MonoBehaviour
+{
+    public CoffeeType type;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
