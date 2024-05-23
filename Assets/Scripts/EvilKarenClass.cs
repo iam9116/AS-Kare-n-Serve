a0d5f7e53a64905d0a0862e9b2b00f3c346969e6
I Love Iced Coffee <148917437+iam9116@users.noreply.github.com>
2024-05-23 17:48:57 -0400

    fghdgh
    rthetyh

---
+++ Assets/Scripts/EvilKarenClass.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EvilKarenClass : MonoBehaviour
+{
+    public GameObject EvilKaren;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            ScoreManager.score -= 1;
+            Invoke("EvilKarenBurner", 1f);
+        }
+    }
+    private void EvilKarenBurner()
+    {
+        Destroy(EvilKaren);
+    }
+}
---
+++ Assets/Scripts/EvilKarenCreator.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EvilKarenCreator : MonoBehaviour
+{
+    public EvilKarenClass[] evilkarens;
+    public int karenToSpawn;
+    public float radiusOfSpawn;
+    public float initialSpawn;
+    public float spawnRate;
+
+    private float xOffset = 0f;
+    private float yOffset = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("MakeMoreKarens", initialSpawn, spawnRate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void MakeMoreKarens()
+    {
+        //if (karenToSpawn <= 0) karenToSpawn = 1;
+        //if (radiusOfSpawn < 1f) radiusOfSpawn = 20f;
+
+        //if (xOffset > 150f)
+        //{
+        //    xOffset = 0f;
+        //    yOffset += 50f;
+        //}
+
+        //if (yOffset > 80f)
+        //{
+        //    yOffset = -92;
+        //    xOffset = 0;
+        //}
+
+
+
+        for (int t = 0; t < karenToSpawn; t++)
+        {
+            //Get random number
+            int randomIndex = Random.Range(0, evilkarens.Length);
+
+            //Get random karen based on random number
+            EvilKarenClass randomObject = evilkarens[randomIndex];
+
+
+            //Figure out where to position karen
+            Vector3 position = new Vector3(xOffset, yOffset, 0);
+
+            //Spawn karen
+            Instantiate(evilkarens[randomIndex], position, Quaternion.identity);
+
+            xOffset += 50f;
+        }
+    }
+
+}
