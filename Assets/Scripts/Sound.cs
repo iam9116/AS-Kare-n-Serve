d53a85360822bb3fc8cb9d282e62945781b1bae6
CodeNinjasEtobicoke <148917437+iam9116@users.noreply.github.com>
2024-06-20 18:01:09 -0400

    fgdf
    dfbdfgh

---
+++ Assets/Scripts/Sound.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound : MonoBehaviour
+{
+    private AudioSource Soundd;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void PlaySoundWhenClicked()
+    {
+        
+    }
+}
