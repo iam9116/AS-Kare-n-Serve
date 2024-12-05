10fffa879ee5031eade9c1e85009f09d864b8fa0
CodeNinjasEtobicoke <148917437+iam9116@users.noreply.github.com>
2024-12-05 18:01:12 -0500

    QWERTYUIOPASDFGHJKLZXCVBNM
    web gl

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEditor.SceneManagement;
 using UnityEngine.SceneManagement;
 
 public class Timer : MonoBehaviour
